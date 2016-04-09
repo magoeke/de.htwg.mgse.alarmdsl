a945df394296941ac2dacbadf093faed529b3be1
magoeke <magoeke@htwg-konstanz.de>
2016-04-09 12:52:10 +0200

    Initial Commit.

---
+++ src/alarmclock/AlarmClock.java
@@ -0,0 +1,15 @@
+package alarmclock;
+
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+
+public final class AlarmClock {
+	private LinkedList<GregorianCalendar> allAlarms;
+	
+	public AlarmClock() {
+		allAlarms = new LinkedList<GregorianCalendar>();
+	}
+	
+	
+	
+}
