3004f33fd43dfad8c1cb4750871e691edd9f64d9
Michael Walz <Michael Walz>
2016-04-11 17:26:56 +0200

    dynamic semantic checks (checks for alarm-in-past and invalid-arguments)

--- src/alarmclock/AlarmProperties.java
+++ src/alarmclock/AlarmProperties.java
@@ -7,44 +7,50 @@ import java.util.LinkedList;
 import alarmclock.AlarmBuilder.WeekAndDayMemorie;
 
 public class AlarmProperties {
-	private int year;
-	private int month;
-	private int day;
-	private int hour;
-	private int minute;
-	private int weeks;
-	private LinkedList<Day> days;
-	
-	public AlarmProperties(GregorianCalendar c, WeekAndDayMemorie wadm) {
-		days = new LinkedList<Day>();
-		year = c.get(Calendar.YEAR);
-		month = c.get(Calendar.MONTH)+1;
-		day = c.get(Calendar.DAY_OF_MONTH);
-		hour = c.get(Calendar.HOUR_OF_DAY);
-		minute = c.get(Calendar.MINUTE);
-		weeks = 0;
-		
-		if(wadm != null) {
-			weeks = wadm.getWeeks();
-			days = wadm.getDays();
-		} else {
-			int day = c.get(Calendar.DAY_OF_WEEK);
-			days.add(Day.values()[(day -2 < 0 ) ? 6 : day-2]);
-		}
-	}
-	
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("Year: ").append(year).append(" Month: ").append(month);
-		sb.append(" Day: ").append(day).append(" Hour: ").append(hour);
-		sb.append(" Minute: ").append(minute).append(" Weeks: ").append(weeks).append(" Days: ");
-		
-		for(Day day: days) {
-			sb.append(day).append(" ");
-		}
-		
-		return sb.toString();
-	}
+    private int year;
+    private int month;
+    private int day;
+    private int hour;
+    private int minute;
+    private int weeks;
+    private LinkedList<Day> days;
+
+    public AlarmProperties(GregorianCalendar c, WeekAndDayMemorie wadm) {
+
+        // dynamic semantic check: check if alarm lies in the past
+        if (c.before(new GregorianCalendar())) {
+            throw new IllegalArgumentException("Alarm lies in the past.");
+        }
+
+        days = new LinkedList<Day>();
+        year = c.get(Calendar.YEAR);
+        month = c.get(Calendar.MONTH) + 1;
+        day = c.get(Calendar.DAY_OF_MONTH);
+        hour = c.get(Calendar.HOUR_OF_DAY);
+        minute = c.get(Calendar.MINUTE);
+        weeks = 0;
+
+        if (wadm != null) {
+            weeks = wadm.getWeeks();
+            days = wadm.getDays();
+        } else {
+            int day = c.get(Calendar.DAY_OF_WEEK);
+            days.add(Day.values()[(day - 2 < 0) ? 6 : day - 2]);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Year: ").append(year).append(" Month: ").append(month);
+        sb.append(" Day: ").append(day).append(" Hour: ").append(hour);
+        sb.append(" Minute: ").append(minute).append(" Weeks: ").append(weeks).append(" Days: ");
+
+        for (Day day : days) {
+            sb.append(day).append(" ");
+        }
+
+        return sb.toString();
+    }
 }
 
